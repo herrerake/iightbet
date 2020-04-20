be7ed89a7bf4720d740b55f7e6e8a7a52a520291
Kevin Herrera <1herrerake@gmail.com>
2020-04-20 16:59:47 -0500

    fix

---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="container">
+            <div className="text-center">
+                <h1>
+                    iightBet
+                </h1>
+                Place your bets!
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
