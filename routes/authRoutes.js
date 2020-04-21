644c170faf014d48f4b8222a3bd9108cf9d16c9a
Kevin Herrera <1herrerake@gmail.com>
2020-04-20 20:33:08 -0500

    deleted node_modules and ran npm install to get updated modules. Changed the react scripts version

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,8 @@ module.exports = app => {
             scope: ['profile', 'email']
         })
     );
-    app.get('/auth/google/callback', 
+    app.get(
+        '/auth/google/callback', 
         passport.authenticate('google'),
         (req, res) => {
             res.redirect('/dashboard');
