a7b5def6a5406b9a9d0ef7943d002b4a57d8b353
Kevin Herrera <1herrerake@gmail.com>
2021-01-10 20:38:02 -0600

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter , Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
-import Header from './Header';
-import Landing from './Landing';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Header from "./Header";
+import Landing from "./Landing";
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const NewBet = () => <h2>New Bet</h2>;
 
 class App extends Component {
-    componentDidMount() {
-        this.props.fetchUser();
-    }
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-    render(){
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/dashboard" component={Dashboard} />
-                        <Route path="/bets/new" component={NewBet} />
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/bets/new" component={NewBet} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default connect(null, actions)(App);
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Landing = () => {
-    return (
-        <div className="container">
-            <div className="text-center">
-                <h1>
-                    iightBet
-                </h1>
-                Place your bets!
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="text-center">
+        <h1>iightBet</h1>
+        Place your bets!!!!!
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
